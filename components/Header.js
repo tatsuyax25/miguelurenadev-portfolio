265336dd79e224853104e930514974e4b554388d
Miguel Urena <urena_miguel82@outlook.com>
2024-07-17 13:41:52 -0700

    Worked Header.js

---
+++ components/Header.js
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="w-full p-4 bg-gray-200 dark:bg-gray-900">
+      <h1 className="text-3xl font-bold">Miguel Ureña Dev Portfolio</h1>
+    </header>
+  )
+}
