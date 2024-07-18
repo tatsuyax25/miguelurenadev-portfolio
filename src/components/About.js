651a5c565a411aaa11eea9d2b1970ac0c96cdf32
Miguel Urena <urena_miguel82@outlook.com>
2024-07-18 13:09:17 -0700

    Update About component

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,15 @@
 export default function About() {
   return (
-    <section id="about" className="p-8 bg-gray-100 dark:bg-gray-800">
-      <h2 className="text-2xl font-bold">About Me</h2>
-      <p className="mt-4">
+    <section id="about" className="bg-gray-800 text-center py-20">
+      <h2 className="text-4xl font-extrabold mb-4">About Me</h2>
+      <p className="text-lg">
         As a devoted and driven Software Engineer, I specialize in developing
         innovative and efficient software solutions that drive business success.
         With a strong foundation in both front-end and back-end development, I
         excel in crafting scalable applications using the MERN stack (MongoDB,
         Express, React, Node.js) and other cutting-edge technologies.
       </p>
-      <p className="mt-4">
+      <p className="text-lg">
         My journey in technology has been marked by a commitment to continuous
         learning and a dedication to excellence. From my early days providing
         technical support and managing e-commerce platforms to my current focus
