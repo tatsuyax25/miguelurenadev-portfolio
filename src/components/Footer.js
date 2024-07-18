e2acefa2a462c48667141592960b8505d3e525af
Miguel Urena <urena_miguel82@outlook.com>
2024-07-18 13:17:55 -0700

    Update Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer>
+    <footer className="w-full bg-gray-900 p-4 text-center">
       <p>© 2024 Miguel Ureña Portfolio. All rights reserved.</p>
     </footer>
   );
