c5797dcdccbca88c1d85e4688cc3e1c56f5051bf
Miguel Urena <urena_miguel82@outlook.com>
2024-07-18 13:54:38 -0700

    import DarkModeToggle to Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,45 @@
+import DarkModeToggle from "./DarkModeToggle";
+
 export default function Header() {
   return (
-    <header className="w-full bg-gray-900 p-4 fixed top-0 left-0 z-50">
+    <header className="fixed top-0 left-0 w-full bg-transparent p-4 flex justify-between items-center z-50">
       <div className="container mx-auto flex justify-between items-center">
-        <h1 className="text-3xl font-bold">Miguel Ureña Dev Portfolio</h1>
-        <nav>
-          <a href="#about" className="mx-4 transition-colors duration-300 hover:text-blue-300">About</a>
-          <a href="#projects" className="mx-4">Projects</a>
-          <a href="#contact" className="mx-4">Contact</a>
+        {/* Title */}
+        <h1 className="text-3xl font-bold text-white">MiguelUreña.Dev</h1>
+
+        {/* Navigation Links */}
+        <nav className="flex space-x-4">
+          <a
+            href="#about"
+            className="text-white transition-colors  hover:text-gray-300"
+          >
+            About
+          </a>
+          <a
+            href="#projects"
+            className="text-white transition-colors  hover:text-gray-300"
+          >
+            Projects
+          </a>
+          <a
+            href="#contact"
+            className="text-white transition-colors  hover:text-gray-300"
+          >
+            Contact
+          </a>
         </nav>
+        {/* Resume Button */}
+        <a
+          href="" // Replace with the path to your resume
+          target="_blank"
+          rel="noopener noreferrer"
+          className="ml-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition-colors"
+        >
+          Resume
+        </a>
+
+        {/* Dark Mode Toggle */}
+        <DarkModeToggle />
       </div>
     </header>
   );
