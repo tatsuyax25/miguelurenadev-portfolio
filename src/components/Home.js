bfdec5d829791ba4aeecfc9c9db82934b9e4a69b
Miguel Urena <urena_miguel82@outlook.com>
2024-08-27 14:11:17 -0700

    Update Home.js for dark mode

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
 export default function HomeSection() {
   return (
-    <section id="home" className="min-h-screen flex items-center justify-center bg-gray-900 text-white">
+    <section id="home" className="min-h-screen flex items-center justify-center bg-gray-100 dark:bg-gray-800 text-black dark:text-white">
       <div className="text-center">
         <h2 className="text-4xl font-bold mb-4">Welcome to Miguel Ureña&apos;s Portfolio</h2>
         <p className="text-xl">A Full-Stack Software Engineer</p>
