ccf13637f1d31b58ff02e7eecd84a3b789ed2fe2
Miguel Urena <urena_miguel82@outlook.com>
2024-08-28 11:42:19 -0700

    Updating Project.js for dark mode

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -26,7 +26,7 @@ export default function Projects() {
     },
   ];
   return (
-    <section id="projects" className="p-8 bg-gray-900 text-white">
+    <section id="projects" className="p-8 bg-gray-100 dark:bg-gray-800 dark:text-white">
       <h2 className="text-4xl font-bold mb-8 text-center">Projects</h2>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
         {projects.map((project, index) => (
