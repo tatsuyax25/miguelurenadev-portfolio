d8b4401975a1bfd87d71dfb81a8e79d92db6641a
Miguel Urena <urena_miguel82@outlook.com>
2024-10-23 12:03:35 -0700

    Fixing email problem

--- src/pages/api/sendEmail.js
+++ src/pages/api/sendEmail.js
@@ -4,12 +4,12 @@ export default async function handler(req, res) {
   if (req.method === "POST") {
     const { name, email, subject, message } = req.body;
 
-    // Configure your Nodemailer transporter
+    // Configure your Nodemailer transporter for Gmail
     const transporter = nodemailer.createTransport({
-      service: "gmail", // You can also try using "hotmail" instead of "Outlook" if needed
+      service: "gmail", // You can also try using any other service like "hotmail", "outlook", etc.
       auth: {
-        user: "mrurena82@gmail.com",
-        pass: "jrtk wzmb dlgq vnmq",
+        user: "mrurena82@gmail.com", // Your Gmail address
+        pass: "gddi nodc heep qxje", // App password or Gmail password (if less secure apps are enabled)
       },
     });
 
@@ -28,7 +28,7 @@ export default async function handler(req, res) {
       res.status(200).json({ message: "Email sent successfully!" });
     } catch (error) {
       console.error("Error sending email:", error);
-      res.status(500).json({ error: "Error sending email." });
+      res.status(500).json({ error: "Error sending email.", details: error });
     }
   } else {
     res.status(405).json(({ message: "Method not allowed" }));
