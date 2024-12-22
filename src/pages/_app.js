ad2f48270426c1b331b2a2a0c47820178d3761b1
Miguel Urena <urena_miguel82@outlook.com>
2024-12-22 12:17:50 -0800

    Adding Google Analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,32 @@
+import { useEffect } from "react";
 import "../styles/globals.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-
-
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    // Initialize Google Analytics
+    const script = document.createElement("script");
+    script.src = "https://www.googletagmanager.com/gtag/js?id=G-CQ8CKJ4TK8";
+    script.async = true;
+    document.head.appendChild(script);
+
+    script.onload = () => {
+      window.dataLayer = window.dataLayer || [];
+      function gtag() {
+        window.dataLayer.push(arguments);
+      }
+      gtag("js", new Date());
+      gtag("config", "G-CQ8CKJ4TK8"); // Replace with your actual Measurement ID
+    };
+
+    return () => {
+      // Cleanup by removing the script when component unmounts
+      document.head.removeChild(script);
+    };
+  }, []);
+
   return <Component {...pageProps} />;
 }
 
