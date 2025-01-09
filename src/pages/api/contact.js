b8912677a85d17ead5f391e0f9ad8e39efe57ab7
Miguel Urena <urena_miguel82@outlook.com>
2025-01-09 11:51:49 -0800

    Update contact.js

--- src/pages/api/contact.js
+++ src/pages/api/contact.js
@@ -47,6 +47,10 @@ export default async function handler(req, res) {
       res.status(200).json({ message: "Message sent successfully!" });
     } catch (error) {
       console.error("Error in sending email:", error);
+
+      // More detailed error logging
+      console.error("Error details:", error.response || error.message || error);
+      
       res
         .status(500)
         .json({ message: "Failed to send message", error: error.message });
