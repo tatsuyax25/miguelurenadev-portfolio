932def90cd16359e8b53997e9af77611a4e1f00c
Miguel Urena <urena_miguel82@outlook.com>
2025-01-13 13:32:31 -0800

    Update Project.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,9 @@
+import Image from "next/image";
+
 export default function Projects() {
   const projects = [
     {
+      id: 1,
       title: "Plantivity",
       description:
         "Plantivity motivates users by completing their daily self-care tasks and rewarding them with a badge. They assign themselves their three daily tasks and points, which help your self-care plant grow!",
@@ -9,6 +12,7 @@ export default function Projects() {
       image: "/images/PlantivityV2.png",
     },
     {
+      id: 2,
       title: "Pocket Web Calculator",
       description:
         "A responsive and accessible calculator built with HTML, CSS, and JavaScript. This project provides a clean user interface and performs basic arithmetic operations.",
@@ -17,6 +21,7 @@ export default function Projects() {
       image: "/images/simplecalculator.png",
     },
     {
+      id: 3,
       title: "Snake Web Game JS",
       description:
         "Navigate the snake to eat food, avoid walls and its own body, and achieve the highest score possible. Each piece of food increases the snake's length, adding to the challenge.",
@@ -25,6 +30,7 @@ export default function Projects() {
       image: "/images/snakeWebGameJS.png",
     },
     {
+      id: 4,
       title: "Connect 4 Web Game",
       description:
         "Connect Four is a game you play against the computer. Take turns dropping colored discs into a vertical grid. The first to get four of their discs in a row, either horizontally, vertically, or diagonally, wins the game.",
@@ -41,29 +47,40 @@ export default function Projects() {
     >
       <h2 className="text-4xl font-bold mb-8 text-center">Projects</h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-        {projects.map((project, index) => (
-          <div
-            key={index}
+        {projects.map((project) => (
+          <article
+            key={project.id}
             className="bg-white dark:bg-gray-800 border border-gray-200 dark:border-gray-700 p-6 rounded-lg shadow-lg transition-shadow duration-300"
           >
-            <img
+            <Image
               src={project.image}
-              alt={project.title}
-              className="w-full h-48 object-cover rounded-md mb-4"
+              alt={`${project.title} Screenshot`}
+              layout="responsive"
+              width={400} // Adjust width based on your layout
+              height={300} // Adjust height based on your layout
+              className="rounded-md mb-4"
             />
             <div className="space-y-4">
               <h3 className="text-2xl font-semibold">{project.title}</h3>
               <p className="text-lg">{project.description}</p>
+              <a
+                href={project.live}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-500 hover:text-blue-700 transition-colors"
+              >
+                Live Demo
+              </a>
               <a
                 href={project.link}
                 target="_blank"
                 rel="noopener noreferrer"
                 className="text-blue-500 hover:text-blue-700 transition-colors"
               >
-                View Project
+                GitHub Repo
               </a>
             </div>
-          </div>
+          </article>
         ))}
       </div>
     </section>
