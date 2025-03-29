fd58c54cbe10a718e14d2c3e7eb42178d62e67c5
Miguel Urena <urena_miguel82@outlook.com>
2025-03-29 13:49:18 -0700

    Fix footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="w-full bg-gray-100 dark:bg-gray-900 text-black dark:text-white p-4 text-center">
+    <footer className="w-full bg-gray-100 dark:bg-gray-900 text-black dark:text-white p-2 text-center">
       <div className="border-t border-gray-300 dark:border-gray-700 mb-4"></div>
       <p className="text-sm sm:text-base">
         © 2025 Miguel Ureña Portfolio. All rights reserved.
