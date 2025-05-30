e9227c23e96f80a973356bc37559b49ae67b5853
Miguel Urena <urena_miguel82@outlook.com>
2025-05-30 09:56:56 -0700

    New screenshot for Plantivity project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -30,7 +30,7 @@ export default function Projects() {
         "Plantivity motivates users by completing their daily self-care tasks and rewarding them with a badge. They assign themselves their three daily tasks and points, which help your self-care plant grow!",
       live: "https://plantivity.netlify.app",
       link: "https://github.com/tatsuyax25/team6-hackathon-fe",
-      image: "/images/PlantivityV2.png",
+      image: "/images/Plantivityv3.png",
     },
     {
       id: 4,
